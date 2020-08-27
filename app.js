49f73ff29b12cbf7f87e23ecc43ca7d551fee6dd
PixelMaker06 <pixelmaker06@gmail.com>
2020-08-27 17:42:18 +0930

    fixed some merge conflicts

---
+++ app.js
@@ -0,0 +1,26 @@
+function openInfo() {
+    if (document.getElementById('infoText').style.display == "none") {
+
+        document.getElementById('infoText').style.display = "block"
+    } else {
+        document.getElementById('infoText').style.display = "none"
+    }
+}
+
+function openUse() {
+    if (document.getElementById('useText').style.display == "none") {
+
+        document.getElementById('useText').style.display = "block"
+    } else {
+        document.getElementById('useText').style.display = "none"
+    }
+}
+
+function openLinks() {
+    if (document.getElementById('linksText').style.display == "none") {
+
+        document.getElementById('linksText').style.display = "block"
+    } else {
+        document.getElementById('linksText').style.display = "none"
+    }
+}
