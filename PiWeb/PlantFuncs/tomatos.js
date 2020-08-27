f76282ace7b6f3d46c4487ff312a361937ee6e4b
Froggymine <37522923+Froggymine@users.noreply.github.com>
2020-08-27 19:25:30 +1000

    Nobody expects the Spanish Inquisition!

--- PiWeb/PlantFuncs/tomatos.js
+++ PiWeb/PlantFuncs/tomatos.js
@@ -1,7 +1,11 @@
-function tomatos(rainOver50tmr) {
-  if rainOver50tmr == true {
+function tomatos(rainChance, temp, whenPlantedMonth, humidity, dayMostlySunny) {
+  if rainChance >= 50 {
     return 0;
   }
+
 }
 
-export { main };
+export { tomatos };
+
+//https://www.researchgate.net/figure/Monthly-crop-water-need-of-tomato_tbl5_280081995 for tomato water needs (table)
+//http://www.fao.org/3/S2022E/s2022e07.htm refere to section 3.1.3
